c1455580b16b32e33f565cb68ad030c6f8127284
Amanjaiswal24 <aman.kumarjaiswal@allen.in>
2025-01-15 12:27:03 +0530

    generate student report pdf

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	S3_BUCKET = "" // Bucket
-	S3_REGION = "" // Region
+	S3_BUCKET = "my-bucket" // Bucket
+	S3_REGION = "us-east-1" // Region
 )
 
 func main() {
@@ -19,24 +19,136 @@ func main() {
 
 	r.LocalFileAccess(true)
 	//html template path
-	templatePath := "templates/companypdf.html"
+	templatePath := "templates/student_report.html"
 
 	//path for download pdf
-	outputPath := "example.pdf"
+	outputPath := "student_report.pdf"
 
 	//html template data
 	templateData := struct {
-		Project     string
-		Description string
-		Company     string
-		Contact     string
-		Date        string
+		Title           string
+		AcademicSession string
+		Address         string
+		FormNo          string
+		StudentName     string
+		CurrentBatch    string
+		Stream          string
+		CourseNameCode  string
+		StudyCenterCode string
+		Tests           []Test
 	}{
-		Project:     "How to convert pdf and upload AWS S3",
-		Description: "This is the simple HTML to PDF file.",
-		Company:     "Izmir",
-		Contact:     "Elçin Zorlu",
-		Date:        "Turkey",
+		Title:           "Student Performance Report",
+		AcademicSession: "2024-2025",
+		Address:         "SAMYAK, LANDMARK CITY, KUNHARI, BUNDI ROAD, KOTA, KOTA (RAJASTHAN)",
+		FormNo:          "1000355023",
+		StudentName:     "ADITYA SAHU",
+		CurrentBatch:    "MEL6B",
+		Stream:          "PRE-MEDICAL",
+		CourseNameCode:  "ENTHUSIAST ADVANCE PHASE-I B(303361)",
+		StudyCenterCode: "KOTA, LANDMARK CITY, KUNHARI(1015)",
+		Tests: []Test{
+			{
+				TestName:   "MAJOR TEST (M36604460)",
+				TestDate:   "18 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5546,
+				TMode:      "OFFLINE",
+				Physics:    150,
+				Chemistry:  149,
+				Biology:    338,
+				Total:      637,
+				Percentage: 88.47,
+				Percentile: 93.23,
+				TestRank:   "-",
+				AIR:        "-",
+			},
+			{
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			},
+			{
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			}, {
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			}, {
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			}, {
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			}, {
+				TestName:   "MAJOR TEST (M36013940)",
+				TestDate:   "13 Dec-24",
+				Batch:      "MEL6B",
+				SA:         5938,
+				TMode:      "OFFLINE",
+				Physics:    144,
+				Chemistry:  152,
+				Biology:    331,
+				Total:      627,
+				Percentage: 87.08,
+				Percentile: 91.23,
+				TestRank:   "-",
+				AIR:        "-",
+			},
+		},
 	}
 
 	if err := r.ParseTemplateFile(templatePath, templateData); err != nil {
@@ -57,9 +169,25 @@ func main() {
 		Bucket:  S3_BUCKET,
 	}
 
-	err = handler.UploadFile("elcin.pdf", outputPath)
+	err = handler.UploadFile("student_report.pdf", outputPath)
 	if err != nil {
 		log.Fatalf("UploadFile - filename: %v, err: %v", outputPath, err)
 	}
 	log.Println("UploadFile - success")
+}
+
+type Test struct {
+	TestName   string
+	TestDate   string
+	Batch      string
+	SA         int
+	TMode      string
+	Physics    int
+	Chemistry  int
+	Biology    int
+	Total      int
+	Percentage float64
+	Percentile float64
+	TestRank   string
+	AIR        string
 }
